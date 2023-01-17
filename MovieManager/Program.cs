9b437875b35f12f87c538110ffd208852eddecc1
Lonnie Johnson <lonnie.e.johnson@gmail.com>
2023-01-16 20:26:24 -0600

    Added authentication

--- MovieManager/Program.cs
+++ MovieManager/Program.cs
@@ -1,23 +1,73 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using MovieManager.Data;
+using MovieManager.Middleware;
 using MovieManager.Services;
 using MovieManager.Services.Interfaces;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("AppDb")));
 
 
-//Dependency INjection so we kow where to get the information for the object 
+//Dependency Injection so we kow where to get the information for the object 
 builder.Services.AddScoped<IIMDBService, IMDBService>();
 
+//Adding Authentication with JWTBearer
+
+string domain = $"https://{builder.Configuration["Auth0:Domain"]}/";
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = domain;
+    options.Audience = builder.Configuration["Auth0:Audience"];
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        NameClaimType = ClaimTypes.NameIdentifier
+    };
+});
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("read:admin", policy => policy.Requirements.Add(new HasScopeRequirement("read:admin", domain))); 
+});
+builder.Services.AddSingleton<IAuthorizationHandler, HasScopeHandler>(); 
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
+
 
 // Address CORs
 builder.Services.AddCors(options =>
